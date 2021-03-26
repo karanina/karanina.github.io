2f23b448911c5822ea3ac62f813315570007989f
karanina <61443737+karanina@users.noreply.github.com>
2021-03-26 13:58:01 +1300

    update 202103261357

---
+++ Assignment1/script/myScript.js
@@ -0,0 +1,18 @@
+var clientFirstName;
+var clientFamilyName;
+var clientCheckInDate;
+var clientCheckOutDate;
+var clientCooking;
+var clientRecreationEquipment;
+
+function bookingBookingDetails () {
+    clientFirstName = document.getElementById("firstName").value;
+    clientLastName = document.getElementById("lastName").value;
+    clientCheckInDate = document.getElementById("checkInDate").value;
+    clientCheckOutDate = document.getElementById("checkOutDate").value;
+    clientCooking = document.getElementById("cooking").value;
+    clientClientRecreationEquipment = document.getElementById("recreationEquipment").value;
+    if (clientFirstName == "" || clientLastName == "" || clientCheckInDate == "" || clientCheckOutDate == "") {
+        alert("Please fill in all fields.");
+    }
+}
